8e5fd9c219453af71267f4bf8c76ede282381900
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-04-29 20:57:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Bem-vindo ao Projeto React em Parceria com a Rocketseat!
+
+Este repositório contém o projeto que estou desenvolvendo em colaboração com o curso de React da Rocketseat. Estamos trabalhando duro para aplicar tudo o que aprendemos de uma forma prática e envolvente, enquanto buscamos elevar nossas habilidades no desenvolvimento web.
+
+## Como Contribuir
+
+Sinta-se à vontade para explorar o código, compartilhar suas ideias e sugestões ou até mesmo contribuir com melhorias. Aqui estão algumas maneiras de contribuir:
+
+- **Ideias**: Tem alguma ideia interessante? Abra uma issue para discuti-la.
+- **Feedback**: Encontrou um bug ou tem alguma sugestão de melhoria? Adoraríamos ouvir sua opinião!
+- **Pull Requests**: Se você deseja corrigir um problema ou adicionar um recurso, sinta-se à vontade para enviar um pull request. Certifique-se de seguir nossas diretrizes de contribuição.
+
+## Tecnologias Utilizadas:
+
+- **React**: Para a construção da interface do usuário, utilizando componentes reutilizáveis e uma arquitetura de aplicativo eficiente.
+- **Styled Components**: Para estilização com CSS-in-JS, proporcionando uma experiência de desenvolvimento mais flexível e modular.
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,56 @@
+import { FiPlus, FiSearch } from 'react-icons/fi';
+
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+
+import { Note } from '../../components/Note';
+import { Input } from '../../components/Input';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
+
+      <Header></Header>
+
+      <Menu>
+        <li>
+          <ButtonText title="Todos" />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+        <li>
+          <ButtonText title="Nodejs" />
+        </li>
+      </Menu>
+
+      <Search>
+        <Input placeholder="Pesquisar pelo titulo" icon={FiSearch} />
+      </Search>
+
+      <Content>
+        <Section title="Minhas notas">
+          <Note
+            data={{
+              title: 'React',
+              tags: [
+                { id: '1', name: 'React' },
+                { id: '2', name: 'rockeseat' }
+              ]
+            }}
+          />
+        </Section>
+      </Content>
+
+      <NewNote>
+        <FiPlus />
+        Criar nota
+      </NewNote>
+    </Container>
+  );
+}
