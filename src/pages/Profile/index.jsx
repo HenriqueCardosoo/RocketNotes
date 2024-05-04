f47089356392eeb32a56a5dcef15ecfcf046cf40
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-05-03 22:23:57 -0300

    add img

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,9 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
-import { Container, Form } from './styles';
+import { Container, Form, Avatar } from './styles';
 
 export function Profile() {
   return (
@@ -14,10 +14,17 @@ export function Profile() {
         </a>
       </header>
       <Form>
-        <Input placeholder="Nome" type="text" icon="FiUser" />
-        <Input placeholder="E-mail" type="text" icon="FiMail" />
-        <Input placeholder="Senha" type="password" icon="FiLock" />
-        <Input placeholder="Nova Senha" type="password" icon="FiLock" />
+        <Avatar>
+          <img src="https://github.com/henriquecardosoo.png" alt="Foto do usuario" />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input placeholder="Nova Senha" type="password" icon={FiLock} />
 
         <Button title="Salvar" />
       </Form>
