c83bef36776462ec771cd9bf927488f58a4d8290
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-05-06 21:35:08 -0300

    adicionando navegações

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,5 @@
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -11,7 +12,7 @@ export function SignUp() {
       <Background />
       <Form>
         <h1>Rocket Notes</h1>
-        <p>Aplicação para salvaer e gerenciar seus links úteis.</p>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
         <h2>Crie sua conta</h2>
 
@@ -21,7 +22,7 @@ export function SignUp() {
 
         <Button title="Cadastrar" />
 
-        <a href="#">Voltar para o login</a>
+        <Link to="/">Voltar para o login</Link>
       </Form>
     </Container>
   );
