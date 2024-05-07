950b8b5e37d3e01d2368defd24446fce22b11b74
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-05-06 21:12:50 -0300

    new finalizado

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,9 @@
+import { Textarea } from '../../components/Textarea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
-import { Textarea } from '../../components/Textarea';
 
 import { Container, Form } from './styles';
 
@@ -17,6 +20,20 @@ export function New() {
 
           <Input placeholder="Titulo" />
           <Textarea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://rockeseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
