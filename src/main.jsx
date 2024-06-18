0e2c2c1fea0662bfe3062e3b671b10715994a2e3
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-06-17 21:26:11 -0300

    criando contexto

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
-import theme from './styles/theme';
+
+import { MyContext } from './myContext';
 
 import { Routes } from './routes';
+
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <MyContext.Provider value={{ name: 'Henrique Cardoso', email: 'henrique@gmail.com' }}>
+        <Routes />
+      </MyContext.Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
