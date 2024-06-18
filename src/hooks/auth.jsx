aee8ab7ec6ebc71c9d0bc9da0d8c9cf8b7df5b8d
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-06-18 00:06:20 -0300

    SignIn

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,9 +1,27 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+
+import { api } from '../services/api';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-  return <AuthContext.Provider value={{ name: 'Henrique Cardoso', email: 'henrique@gmail.com' }}>{children}</AuthContext.Provider>;
+  const [data, setData] = useState({});
+
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post('/sessions', { email, password });
+      const { user, token } = response.data;
+
+      api.defaults.headers.authorization = `bearer ${token}`;
+      setData({ user, data });
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else 'Não foi possivel entrar.';
+    }
+  }
+
+  return <AuthContext.Provider value={{ signIn, user: data.user }}>{children}</AuthContext.Provider>;
 }
 
 function useAuth() {
